72990dfa1b28bffdbac7469c0c6c2929b2e1e2cc
Gage Miller <gmillerwork3@gmail.com>
2021-10-11 23:37:58 -0500

    Added Basic GUI code

---
+++ Components/GUI.py
@@ -0,0 +1,13 @@
+#import LoadCell
+import tkinter as tk
+import matplotlib.pyplot as plt
+import csv, time
+import cv2
+
+root = tk.Tk()
+
+txt = tk.Label(root, text="HU Hybrid Rocket Engine Data Acquisition Software")
+
+txt.pack()
+
+root.mainloop()
