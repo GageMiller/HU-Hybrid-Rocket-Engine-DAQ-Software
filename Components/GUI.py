9b882c855221c546ef5cd79b100597f4aec73569
Gage Miller <gmillerwork3@gmail.com>
2021-10-12 22:10:11 -0500

    Added basic live graph implementation

--- Components/GUI.py
+++ Components/GUI.py
@@ -1,13 +1,46 @@
 #import LoadCell
-import tkinter as tk
-import matplotlib.pyplot as plt
+from tkinter import *
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import csv, time
 import cv2
 
-root = tk.Tk()
+class window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        
+root = Tk()
+app = window(master=root)
+app.master.geometry('1200x700+200+100')
+app.master.title("Hybrid Rocket Engine Data Acquisition Software")
+#loadCellTxt = Label(root, text="Load Cell")
+#loadCellTxt.pack()
 
-txt = tk.Label(root, text="HU Hybrid Rocket Engine Data Acquisition Software")
+xar = []
+yar = []
 
-txt.pack()
+style.use('ggplot')
+fig = plt.figure(figsize=(14, 4.5), dpi=100)
+ax1 = fig.add_subplot(1, 1, 1)
+ax1.set_ylim(0, 100)
+line, = ax1.plot(xar, yar, 'r', marker='o')
+#ser = serial.Serial('com3', 9600)
+
+def animate(i):
+    #ser.reset_input_buffer()
+    #data = ser.readline().decode("utf-8")
+    #data_array = data.split(',')
+    #yvalue = float(data_array[1])
+    yar.append(99-i)
+    xar.append(i)
+    line.set_data(xar, yar)
+    ax1.set_xlim(0, i+1)
+
+
+plotcanvas = FigureCanvasTkAgg(fig, app.master)
+plotcanvas.get_tk_widget().grid(column=1, row=1)
+ani = animation.FuncAnimation(fig, animate, interval=1000, blit=False)
 
 root.mainloop()
