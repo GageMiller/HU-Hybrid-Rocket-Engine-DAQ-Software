edf7898c712ec2ab4cb17ba9b7f4c9699a4122f8
Gage Miller <gmillerwork3@gmail.com>
2021-11-01 22:55:36 -0500

    Created Camera.py

---
+++ Components/Camera.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageTk
+import cv2
+
+
+
+# Define function to show frame
+def show_frames(label, cap):
+   # Get the latest frame and convert into Image
+   cv2image= cv2.cvtColor(cap.read()[1],cv2.COLOR_BGR2RGB)
+   img = Image.fromarray(cv2image)
+   # Convert image to PhotoImage
+   imgtk = ImageTk.PhotoImage(image = img)
+   label.imgtk = imgtk
+   label.configure(image=imgtk)
+   # Repeat after an interval to capture continiously
+   label.after(20, show_frames)
